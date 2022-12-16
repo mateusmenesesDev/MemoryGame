317e2c97b17537c49c53ffe3f5d1a4114cb49bb8
Mateus <mateusppa10@gmail.com>
2022-12-16 16:15:06 -0300

    Limpando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import './App.css';
-import { Header } from './components/Header';
-
 function App() {
-  const name = 'Mateus';
-  return <Header name={name} />;
+  return <div></div>;
 }
 
 export default App;
--- src/components/Header/index.jsx
+++
@@ -1,3 +0,0 @@
-export function Header({ name }) {
-  return <div>{name}</div>;
-}
